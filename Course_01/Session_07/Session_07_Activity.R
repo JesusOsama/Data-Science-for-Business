a06a92050256962faa77466fc89df7925f5876d8
JesusOsama <jesusv2911@gmail.com>
2024-10-22 10:26:18 -0500

    Eliminar Actividad Pokemon Course 01 Session 07

--- Course_01/Session_07/Session_07_Activity.R
+++ Course_01/Session_07/Session_07_Activity.R
@@ -28,20 +28,3 @@ plot(tree.student_data)
 text(tree.student_data, pretty = 0)
 
 tree.student_data
-
-#### Activity 2 ####
-
-## Load the Pokemon Dataset
-Pokemon <- read.csv("./Course_01/Session_07/PokemonData.csv")
-
-## Explore Data
-Pokemon$Legendary <- factor(Pokemon$Legendary, levels = c(TRUE, FALSE), labels = c("Legendary","Not_Legendary"))
-names(Pokemon)
-head(Pokemon)
-
-## Decision Tree
-tree.Pokemon = tree(Legendary~HP+Attack+Defense+SpAtk+SpDef+Speed+Generation, data=Pokemon)
-summary(tree.Pokemon)
-
-plot(tree.Pokemon)
-text(tree.Pokemon, pretty=0)
